612f11cbf22a72fccef035cf1c278d6e6271fe20
Coding-Cooking <fei.coding2023@gmail.com>
2024-09-18 20:53:33 +1000

    page visit count

--- src/tcshome/views.py
+++ src/tcshome/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from visits.models import PageVisit
 
 def home_page_view(request, *args, **kwargs):
-    queryset = PageVisit.objects.all()
+    # qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My_page"
     my_context = {
         "page_title": my_title,
-        "page_visit_count": queryset.count()
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": PageVisit.objects.count()
     }
-    path = request.path
-    print(path)
+    print(f"Page path: {request.path}")
     html_template = "home.html"
-    PageVisit.objects.create()
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
