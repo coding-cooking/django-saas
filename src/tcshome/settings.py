0867975f175b8b91bee1ba9a63fd8d30132c229b
Coding-Cooking <fei.coding2023@gmail.com>
2024-09-19 20:21:42 +1000

    add django debug variable

--- src/tcshome/settings.py
+++ src/tcshome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oyv8nfdh6r_b(w0(d+w4$j^0s(+0=o*_c)o$-114#!d8r3p+j)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")) == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app"  # https://saas.prod.ailway.app
