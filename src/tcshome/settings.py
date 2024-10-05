a233834b405a826f0c3572d8defe06f5c038bc2c
Coding-Cooking <fei.coding2023@gmail.com>
2024-10-05 15:27:28 +1000

    add whitenoise

--- src/tcshome/settings.py
+++ src/tcshome/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +149,14 @@ STATICFILES_DIRS = [
 
 # output for python manage.py collectstatic
 STATIC_ROOT = BASE_DIR / 'local-cdn'
+# this is for < Django 4.2:
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
