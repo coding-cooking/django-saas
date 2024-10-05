1df57d86696dca9c73f731d7f32c93e1c458d157
Coding-Cooking <fei.coding2023@gmail.com>
2024-10-05 13:16:25 +1000

    add commando app and verdor_pull management command

--- src/tcshome/settings.py
+++ src/tcshome/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commando',
     'visits',
 ]
 
@@ -136,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-# STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # source(s) for python manage.py collectstatic
